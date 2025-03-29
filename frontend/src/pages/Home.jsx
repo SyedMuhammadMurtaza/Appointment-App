60478ee672ee007a6d34e6763e4f8ab0b30f4910
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-03-29 21:52:53 +0500

    Header section created and added to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-black'>Home</h1>
+    <div className='mt-10'>
+      <Header/>
     </div>
   )
 }
