461dc278daf53dfcfdea3842c7597ac5ab319833
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-04-04 01:28:29 +0500

    configurations updatd for netlify deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // https://vite.dev/config/
   plugins: [react()],
-  base: './', // Ensure correct relative paths
+  base: '/', // Ensure correct asset paths
 })
